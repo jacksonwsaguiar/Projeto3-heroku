083c69c038497d8294dc898800077f11f818c7d0
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2022-06-21 15:28:42 -0300

    test route deploy

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ require("./routes/index")(app);
 
 app.use(express.static("views"));
 // app.get("/dashboard", (req, res)=>{res});
-app.get("/dashboard", () => app.render(express.static("/Dashboard")));
-app.get("/profile", express.static("/Profile"));
-app.get("/hurbcontrol", express.static("/HurbControl"));
-app.get("/authentication", express.static("/Authentication"));
+app.use("/dashboard", express.static(__dirname + "/Dashboard"));
+app.use("/profile", express.static(__dirname + "/Profile"));
+app.use("/hurbcontrol", express.static(__dirname + "/HurbControl"));
+app.use("/authentication", express.static(__dirname + "/Authentication"));
 
 app.listen(PORT);
