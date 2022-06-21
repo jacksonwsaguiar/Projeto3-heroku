e1f768ca16b58a4009c7b44eb48024ddd0507366
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2022-06-21 14:56:48 -0300

    add route test

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 require("./routes/index")(app);
 
 app.use("/", express.static("views"));
-app.get("/dashboard", express.static("./views/Dashboard/index.html"));
+app.get("/dashboard", (req, res) => {
+  res.json({ message: "teste ok" });
+});
 app.get("/profile", express.static("/Profile"));
 app.get("/hurbcontrol", express.static("/HurbControl"));
 app.get("/authentication", express.static("/Authentication"));
